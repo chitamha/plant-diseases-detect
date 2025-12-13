e28744aff20f5b1d7030ff3bd3d7cd28d7974cb2
Chi Tam <2512203934@student.hcmus.edu.vn>
2025-12-13 22:14:16 +0700

    Update new code

--- main.py
+++ main.py
@@ -551,7 +551,7 @@ st.html("""
     /* Hide default streamlit button styling for the chatbot FAB */
     div[data-testid="stVerticalBlock"] > div:last-child button[kind="primary"] {
         position: fixed !important;
-        bottom: 50px !important;
+        bottom: 60px !important;
         right: 20px !important;
         z-index: 9999 !important;
         width: 50px !important;
